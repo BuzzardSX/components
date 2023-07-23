7f6387a75377ea00c6013101dbff0e5aa07361c7
Павел Бобров <buzzard.sx@gmail.com>
2023-07-23 20:16:35 +0500

    main: tr

--- src/components/data/table/head.jsx
+++ src/components/data/table/head.jsx
@@ -5,17 +5,6 @@ const Head = () => (
 	<Consumer>
 		{({ data: rows, columns }) => (
 			<thead>
-				<tr>
-					{columns
-						?
-							columns.map(
-								({ key, title }) => <th key={key}>{title}</th>
-							)
-						:
-							Object.keys(rows[0]).map(
-								key => <th key={key}>{key}</th>
-							)}
-				</tr>
 				<Map
 					items={columns}
 					fallback={
