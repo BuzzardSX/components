a51c9ca74da619450f54e22357e417725f8a06bb
Павел Бобров <buzzard.sx@gmail.com>
2023-07-23 20:23:34 +0500

    main: fallback

--- src/components/data/table/head.jsx
+++ src/components/data/table/head.jsx
@@ -1,18 +1,23 @@
 import { Consumer } from './context'
 import { Map } from '~/root'
 
+const Fallback = () => (
+	<Consumer>
+		{({ data }) =>
+			Object.keys(data[0]).map(
+				key => <th key={key}>{key}</th>
+			)}
+	</Consumer>
+)
+
+const row = ({ key, title }) => <th key={key}>{title}</th>
+
 const Head = () =>
 	<Consumer>
 		{({ data, columns }) =>
 			<thead>
-				<Map
-					items={columns}
-					fallback={
-						Object.keys(data[0]).map(
-							key => <th key={key}>{key}</th>
-						)
-					}>
-					{({ key, title }) => <th key={key}>{title}</th>}
+				<Map items={columns} fallback={<Fallback />}>
+					{row}
 				</Map>
 			</thead>}
 	</Consumer>
