f76a5bad9271e5c7755ba036f802ea4d9ef52a65
Павел Бобров <buzzard.sx@gmail.com>
2023-08-21 03:12:46 +0500

    record

--- src/data.tsx
+++ src/data.tsx
@@ -4,22 +4,21 @@ export namespace Record {
 	export type Key = string
 
 	export type Value = string
+
+	export interface Props<I, K extends Record.Key, V extends Record.Value> {
+		data: I[]
+		$key: (item: I) => K
+		value: (item: I) => V
+	}
+
+	export interface Render<K extends Record.Key, V extends Record.Value> {
+		children: (key: K, value: V) => ReactNode
+	}
+
+	export type Component = <I, K extends Record.Key, V extends Record.Value>({ data, children }: Record.Props<I, K, V> & Record.Render<K, V>) => JSX.Element
 }
 
-interface Props<I, K extends Record.Key, V extends Record.Value> {
-	data: I[]
-	$key: (item: I) => K
-	value: (item: I) => V
-}
-
-interface Render<K extends Record.Key, V extends Record.Value> {
-	children: (key: K, value: V) => ReactNode
-}
-
-type Component = <I, K extends Record.Key, V extends Record.Value>({ data, children }: Props<I, K, V> & Render<K, V>) => JSX.Element
-
-
-const Component: Component = ({ data, $key, value, children: render }) =>
+const Component: Record.Component = ({ data, $key, value, children: render }) =>
 	<>
 		{data.map((item) => render($key(item), value(item)))}
 	</>
