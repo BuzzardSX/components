cd8a0e4e3f3c5ca2003f6f4c5eea91a556e99269
Павел Бобров <buzzard.sx@gmail.com>
2023-08-20 23:29:35 +0500

    render

--- src/data.tsx
+++ src/data.tsx
@@ -1,8 +1,21 @@
-import { Fragment } from 'react'
+import { Fragment, type ReactNode } from 'react'
 
-const Data = () =>
+type Key = string
+
+interface Item<K extends Key> {
+	key: K
+}
+
+interface Props<K extends Key> {
+	items: Item<K>[]
+	itemRender: (key: K) => ReactNode
+}
+
+const Data = <K extends Key>({ items, itemRender }: Props<K>) =>
 	<Fragment>
-		Data
+		{items.map(
+			(item) => itemRender(item.key)
+		)}
 	</Fragment>
 
 export default Data
