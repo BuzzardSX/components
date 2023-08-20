c799a4aaf9ac9df20660c95f5943df10bf6ea9dc
Павел Бобров <buzzard.sx@gmail.com>
2023-08-21 03:40:37 +0500

    data

--- src/data.tsx
+++ src/data.tsx
@@ -17,11 +17,7 @@ export namespace Record {
 
 	export type Component = <I, K extends Key, V extends Value>({ data, children }: Props<I, K, V> & Children<K, V>) => JSX.Element
 
-	export interface Data<I, K extends Key, V extends Value> {
-		data: I[]
-		recordKey: (item: I) => K
-		recordValue: (item: I) => V
-	}
+	export type Data<I, K extends Key, V extends Value> = Omit<Props<I, K, V>, '$key' | 'value'> & { recordKey: Props<I, K, V>['$key'], recordValue: Props<I, K, V>['value'] }
 }
 
 const Component: Record.Component = ({ data, $key, value, children: render }) =>
