729aa2b70ce45eb3b8dba7f350fa20406d32276e
Павел Бобров <buzzard.sx@gmail.com>
2023-08-21 00:18:48 +0500

    data

--- src/data.tsx
+++ src/data.tsx
@@ -4,12 +4,9 @@ type RecordKey = string
 
 type RecordValue = string
 
-interface DataSource<I, K extends RecordKey, V extends RecordValue> {
+interface Props<I, K extends RecordKey, V extends RecordValue> {
 	value: I[]
 	record: [key: (item: I) => K, value: (item: I) => V]
-}
-
-interface Props<I, K extends RecordKey, V extends RecordValue> extends DataSource<I, K, V> {
 	children: (key: K) => ReactNode
 }
 
