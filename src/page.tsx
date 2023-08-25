3b0b46d33077d7e201c16dd8c37ac4afca450d0e
Павел Бобров <buzzard.sx@gmail.com>
2023-08-25 19:51:03 +0500

    color

--- src/page.tsx
+++ src/page.tsx
@@ -1,5 +1,27 @@
-const Page = () =>
-	<div>
-	</div>
+import { useState } from 'react'
+
+type Color = 'red' | 'blue'
+
+const useColor = () => {
+	const [color, setColor] = useState<Color>('red')
+
+	const toggle = () => setColor(color == 'red' ? 'blue' : 'red')
+
+	return {
+		color,
+		toggle
+	}
+}
+
+const Page = () => {
+	const { color, toggle } = useColor()
+
+	return (
+		<div>
+			<button onClick={toggle}>Change color</button>
+			<div style={{ backgroundColor: color, 'height': '400px', 'width': '400px' }}></div>
+		</div>
+	)
+}
 
 export default Page
