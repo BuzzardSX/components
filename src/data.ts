47b944a8daa8d173ccbd3e1ac19f5d4c97775565
Павел Бобров <buzzard.sx@gmail.com>
2023-08-27 23:35:14 +0500

    render

--- src/data.ts
+++ src/data.ts
@@ -2,6 +2,20 @@ export type Key = string | number
 
 export type ChangeEventHandler<K> = (key: K) => void
 
+type Item<K extends Key> = {
+	key: K
+}
+
+interface Args<K extends Key> {
+	items: Item<K>[]
+}
+
+const render = <K extends Key>(args: Args<K>) => {}
+
+render({
+	items: [{ key: 1 }]
+})
+
 export interface Props<K extends Key> {
 	dataKeys: ToArray<K>
 	onChange?: (key: K) => void
