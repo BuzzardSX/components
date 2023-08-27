fe6a292d3d2d7de3546a3ab17b9bd13d46c9f10e
Павел Бобров <buzzard.sx@gmail.com>
2023-08-28 04:45:16 +0500

    orig

--- src/data.ts
+++ src/data.ts
@@ -2,22 +2,6 @@ type Key = string | number
 
 type ChangeEventHandler<K extends Key> = (key: K) => void
 
-type ItemKey = number
-
-type BeforeItem<K> = { key: K }
-
-type DataItem<K> = K extends number ? number extends K ? BeforeItem<K> : never : never
-
-interface Args<K extends ItemKey> {
-	items: DataItem<K>[]
-}
-
-const render = <K extends ItemKey>(args: Args<K>) => {}
-
-render({
-	items: [{ key: 1 }, { key: 2 }]
-})
-
 interface Props<K extends Key> {
 	itemKeys: ToArray<K>
 	onChange?: ChangeEventHandler<K>
