6d3598faf90b2810857e171b0d0dc443c3a40687
Павел Бобров <buzzard.sx@gmail.com>
2023-08-27 15:10:14 +0500

    select

--- src/page.tsx
+++ src/page.tsx
@@ -1,27 +1,26 @@
-import { useState } from 'react'
+type Key = string | number
 
-type Color = 'red' | 'blue'
-
-const useColor = () => {
-	const [color, setColor] = useState<Color>('red')
-
-	const toggle = () => setColor(color == 'red' ? 'blue' : 'red')
-
-	return {
-		color,
-		toggle
-	}
+interface SelectProps<K extends Key> {
+	options: ToArray<K>
+	onChange?: (key: K) => void
 }
 
-const Page = () => {
-	const { color, toggle } = useColor()
+const arr = [[1, 2], ['a', 'b']]
 
+const Select = <T extends Key>({ options, onChange }: SelectProps<T>) => {
 	return (
-		<div>
-			<button onClick={toggle}>Change color</button>
-			<div style={{ backgroundColor: color, 'height': '400px', 'width': '400px' }}></div>
-		</div>
+		<select onChange={(event) => onChange?.(event.target.value as T)}>
+			{options.map(
+				(option) => <option value={option}>{option}</option>
+			)}
+		</select>
 	)
 }
+
+const Page = () => (
+	<div>
+		<Select options={[1, 2, 3]} onChange={(key) => console.log()} />
+	</div>
+)
 
 export default Page
