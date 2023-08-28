c3a221b9e1c783549c4130a19e2dd18182689760
BuzzardSX <buzzard.sx@gmail.com>
2023-08-28 16:14:27 +0500

    item

--- src/data.ts
+++ src/data.ts
@@ -4,8 +4,13 @@ type Value = string | number
 
 type ChangeEventHandler<K extends Key> = (key: K) => void
 
+type Item<K, V> = {
+	key: K
+	value: V
+}
+
 interface Props<K extends Key, V extends Value> {
-	items: K extends unknown ? V extends unknown ? { key: K, value: V }[] : never : never
+	items: K extends unknown ? V extends unknown ? Item<K, V>[] : never : never
 	onChange?: ChangeEventHandler<K>
 }
 
