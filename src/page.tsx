e8b79717783866ef76ebe1a65ec984a97a543bc6
Павел Бобров <buzzard.sx@gmail.com>
2023-09-15 17:25:51 +0500

    page

--- src/page.tsx
+++ src/page.tsx
@@ -1,24 +1,6 @@
-import Select from './select'
-
-const brands = [
-	{
-		key: 1,
-		value: 'LG'
-	},
-	{
-		key: 2,
-		value: 'Samsung'
-	},
-	{
-		key: 3,
-		value: 'Philips'
-	}
-]
-
 const Page = () => {
 	return (
 		<div>
-			<Select items={brands} onChange={(key) => console.log(key)} />
 		</div>
 	)
 }
