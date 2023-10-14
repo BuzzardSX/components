7d0efbc3866617782fce98140ea61da2cc4fa227
Павел Бобров <buzzard.sx@gmail.com>
2023-10-14 10:23:30 +0500

    comm

--- src/data.ts
+++ src/data.ts
@@ -13,15 +13,6 @@ type Item<K, V> = {
 
 type Items<K, V> = K extends unknown ? V extends unknown ? Item<K, V>[] : never : never
 
-namespace Example {
-	/**
-	 * Нужно сделать так, чтобы при использовании компонента, TS мог однозначно определить, относится ли массив items к типу Item, либо нарушает его
-	*/
-	/**
-	 * Точно известно, что поле itemMap будет иметь тип (item: I) => K, где I extends object и K extends Key
-	 */
-}
-
 interface Props<K extends Key, V extends Value> {
 	items: Items<K, V>
 	onChange?: ChangeEventHandler<K>
