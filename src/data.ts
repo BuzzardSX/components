e10eaf2e98d0bba0efcb535b2cefc89fe05ed9b6
Павел Бобров <buzzard.sx@gmail.com>
2023-10-18 01:11:04 +0500

    array

--- src/data.ts
+++ src/data.ts
@@ -7,7 +7,7 @@ export type Value = string | number
 export type ChangeEventHandler<K extends Key> = (key: K) => void
 
 export interface Props<K extends Key, V extends Value> {
-	items: K extends unknown ? V extends unknown ? { key: K, value: V } : never : never
+	items: K extends unknown ? V extends unknown ? { key: K, value: V }[] : never : never
 	onChange?: ChangeEventHandler<K>
 	renderItem?: (value: V) => ReactNode
 }
