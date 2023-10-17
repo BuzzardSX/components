29f1c18dd7a8dc87eea27da3efbf474e7ddbfcc7
Павел Бобров <buzzard.sx@gmail.com>
2023-10-18 01:19:29 +0500

    select

--- src/page.tsx
+++ src/page.tsx
@@ -1,6 +1,11 @@
+import Select from './select'
+
 const Page = () => {
 	return (
 		<div>
+			<Select
+				items={[{ key: 1, value: 'One' }, { key: 2, value: 'Two' }]}
+				onChange={(key) => console.log(key)} />
 		</div>
 	)
 }
