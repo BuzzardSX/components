abe1087c3dd009a2e596c85e40c3ea672ad12abf
Павел Бобров <buzzard.sx@gmail.com>
2023-10-24 17:53:16 +0500

    export

--- src/data.ts
+++ src/data.ts
@@ -1,10 +1,17 @@
-export type Key = string | number
+type Key = string | number
 
-export type Value = string | number
+type Value = string | number
 
-export type ChangeEventHandler<K extends Key> = (key: K) => void
+type ChangeEventHandler<K extends Key> = (key: K) => void
 
-export interface Props<K extends Key, V extends Value> {
+interface Props<K extends Key, V extends Value> {
 	items: K extends unknown ? V extends unknown ? { key: K, value: V }[] : never : never
 	onChange?: ChangeEventHandler<K>
+}
+
+export {
+	Key,
+	Value,
+	ChangeEventHandler,
+	Props
 }
