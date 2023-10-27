090600a2988fcb8a21c3006015d0aa3d1f8d6731
Павел Бобров <buzzard.sx@gmail.com>
2023-10-27 18:39:46 +0500

    items

--- src/data.ts
+++ src/data.ts
@@ -4,11 +4,15 @@ type Value = string | number
 
 type ChangeEventHandler<K extends Key> = (key: K) => void
 
+type Items<K, V> = K extends unknown ? V extends unknown ? { key: K, value: V }[] : never : never
+
 interface Props<K extends Key, V extends Value> {
-	items: K extends unknown ? V extends unknown ? { key: K, value: V }[] : never : never
+	items: Items<K, V>
 	onChange?: ChangeEventHandler<K>
 }
 
-type Result = string extends unknown ? symbol : boolean
+type StringToNull<T> = T extends string ? null : T
+
+type Result = StringToNull<string | number>
 
 export { Key, Value, ChangeEventHandler, Props }
