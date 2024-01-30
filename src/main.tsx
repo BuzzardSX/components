da460d66edc7beb4d67684d637966dea8899ba15
BuzzardSX <buzzard.sx@gmail.com>
2024-01-30 12:27:41 +0500

    format

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import App from './app'
 import { createRoot } from 'react-dom/client'
 
-const app = document.getElementById('app')!;
+const app = document.getElementById('app')!
 
 createRoot(app).render(<App />)
