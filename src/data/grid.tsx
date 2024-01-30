95164e33898e6b649cdea2d2eb21038d0664dae8
BuzzardSX <buzzard.sx@gmail.com>
2024-01-30 13:01:12 +0500

    body

--- src/data/grid.tsx
+++ src/data/grid.tsx
@@ -2,12 +2,18 @@ import type Table from './table'
 
 interface Props<R> extends Table<R> {}
 
-function Component<R>(props: Props<R>) {
+function Component<R>({ records }: Props<R>) {
 	return (
 		<table>
 			<thead>
 			</thead>
 			<tbody>
+				{records.map(
+					(record) =>
+						<tr>
+							<td>Content</td>
+						</tr>
+				)}
 			</tbody>
 		</table>
 	)
