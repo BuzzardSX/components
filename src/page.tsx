db3f366589dd2f8676d48255333dc6bb3aa54242
BuzzardSX <buzzard.sx@gmail.com>
2024-01-30 13:08:07 +0500

    users

--- src/page.tsx
+++ src/page.tsx
@@ -1,8 +1,27 @@
 import Select from './select'
+import { Grid } from './data'
+
+const users = [
+	{
+		name: 'Mike'
+	},
+	{
+		name: 'Tom'
+	}
+]
 
 const Page = () => {
 	return (
 		<div>
+			<div>
+				<Grid
+					definition={{
+						name: {
+							recordIndex: 'name'
+						}
+					}}
+					records={users} />
+			</div>
 			<div>
 				<Select
 					items={[{ key: 1, value: 'One' }, { key: 2, value: 'Two' }]}
