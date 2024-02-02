9d43b949fcb8c897035f83f8cb4c94a2c4e1b07f
Павел Бобров <buzzard.sx@gmail.com>
2024-02-02 23:49:41 +0500

    format

--- src/data/grid.tsx
+++ src/data/grid.tsx
@@ -2,30 +2,27 @@ import type { Table, Record } from './table'
 
 interface Props<R extends Record> extends Table<R> {}
 
-const Component = <R extends Record>({ definition, records }: Props<R>) => {
-	return (
-		<table>
-			<thead>
-				<tr>
-					{Object.entries(definition)
-						.map(
-							([key, { title }]) => <th>{title ?? key}</th>
-						)}
-				</tr>
-			</thead>
-			<tbody>
-				{records.map(
-					(record) =>
-						<tr>
-							{Object.values(definition)
-								.map(
-									({ recordIndex }) => <td>{record[recordIndex]}</td>
-								)}
-						</tr>
-				)}
-			</tbody>
-		</table>
-	)
-}
+const Component = <R extends Record>({ definition, records }: Props<R>) =>
+	<table>
+		<thead>
+			<tr>
+				{Object.entries(definition)
+					.map(
+						([key, { title }]) => <th>{title ?? key}</th>
+					)}
+			</tr>
+		</thead>
+		<tbody>
+			{records.map(
+				(record) =>
+					<tr>
+						{Object.values(definition)
+							.map(
+								({ recordIndex }) => <td>{record[recordIndex]}</td>
+							)}
+					</tr>
+			)}
+		</tbody>
+	</table>
 
 export default Component
