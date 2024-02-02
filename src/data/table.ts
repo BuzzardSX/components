14f218b2fd6fd48f93f1586e62f8c96870a77812
Павел Бобров <buzzard.sx@gmail.com>
2024-02-02 23:48:17 +0500

    export

--- src/data/table.ts
+++ src/data/table.ts
@@ -3,20 +3,15 @@ type DefinitionValue<R> = {
 	title?: string
 }
 
-type Record = {
-	[key: string]: string
-}
-
 type Definition<R> = {
 	[key: string]: DefinitionValue<R>
 }
 
-interface Table<R extends Record> {
-	definition: Definition<R>
-	records: R[]
+export type Record = {
+	[key: string]: string
 }
 
-export type {
-	Table,
-	Record
+export interface Table<R extends Record> {
+	definition: Definition<R>
+	records: R[]
 }
